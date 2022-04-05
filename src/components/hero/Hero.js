9e9e949e469f420c4e78de5d4e12e42be7a76ae7
Christopher Shoo <rishaelibenson@gmail.com>
2022-04-05 12:26:32 +0300

    fix overflowing views not clipping

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,8 +3,8 @@ import MovieName from 'components/hero/MovieName';
 
 const Hero = () => {
     return (
-        <section className="flex h-52 md:h-80">
-            <div className="h-full w-full bg-brand-900 rounded-xl overflow-clip relative">
+        <section className="flex h-52 md:h-80 overflow-hidden">
+            <div className="h-full w-full bg-brand-900 rounded-xl overflow-hidden relative">
                 <div className="h-full w-full bg-gradient-to-t from-gray-900 flex flex-col justify-end px-5 pb-3 absolute md:px-8">
                     <div className="flex items-end justify-between pb-4">
                         <MovieName />
--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -2,7 +2,7 @@ import MovieName from "components/movies/MovieName";
 
 const Movie = () => {
     return (
-        <div className="bg-gray-100 rounded-lg overflow-clip relative">
+        <div className="bg-gray-100 rounded-lg overflow-hidden relative">
             <div className="w-full p-5 bottom-0 backdrop-blur-sm bg-white/10 absolute">
                 <MovieName />
             </div>
